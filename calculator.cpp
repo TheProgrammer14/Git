b5174df160c13bf958cd9300478e68858f3fdc16
CJ Katigbak <cjkatigbak14@gmail.com>
2022-03-30 14:19:59 +0800

    Initial Commit This is my first time to commit using Git.

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+/*
+	Name: Christian Jericho Katigbak
+	Copyright: 
+	Author: 
+	Date: 09/09/21 16:54
+	Description: Interim1Katigbak
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+ int firstnumber, secondnumber, sumoftwonumber;
+
+ cout<<"Enter First Number: ";
+ cin>>firstnumber;
+
+cout<<"Enter Second Number: ";
+cin>>secondnumber;
+
+
+sumoftwonumber=firstnumber+secondnumber;
+
+cout<<endl<<firstnumber<<" + "<<secondnumber<<" = "<< sumoftwonumber;
+
+return 0;
+}

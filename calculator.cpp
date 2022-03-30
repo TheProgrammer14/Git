579eff655fdf888dc04bd78d5c3b846f0127d8bb
CJ Katigbak <cjkatigbak14@gmail.com>
2022-03-30 15:35:33 +0800

    Added modulo feature.

--- calculator.cpp
+++ calculator.cpp
@@ -6,10 +6,14 @@
 	Description: Interim1Katigbak
 */
 #include <iostream>
+
+
 using namespace std;
 
 int main(){
- int firstnumber, secondnumber, sumoftwonumber, diffoftwonumbers, quotwonumbers,prooftwonumbers;
+
+
+ int firstnumber, secondnumber, sumoftwonumber, diffoftwonumbers, quotwonumbers,prooftwonumbers, modulooftwonumbers;
 
  cout<<"Enter First Number: ";
  cin>>firstnumber;
@@ -21,12 +25,14 @@ sumoftwonumber=firstnumber+secondnumber;
 diffoftwonumbers=firstnumber-secondnumber;
 prooftwonumbers=firstnumber*secondnumber;
 quotwonumbers=firstnumber/secondnumber;
+modulooftwonumbers=firstnumber%secondnumber;
 
 cout<<endl<<firstnumber<<" + "<<secondnumber<<" = "<< sumoftwonumber;
 cout<<endl<<firstnumber<<" - "<<secondnumber<<" = "<< diffoftwonumbers;
 cout<<endl<<firstnumber<<" * "<<secondnumber<<" = "<< prooftwonumbers;
 
 cout<<endl<<firstnumber<<" / "<<secondnumber<<" = "<< quotwonumbers;
+cout<<endl<<firstnumber<<" % "<<secondnumber<<" = "<< modulooftwonumbers;
 
 return 0;
 }
